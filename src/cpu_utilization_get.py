f8e73b0c6e2474ef548d484f0f69ade78eac0de0
Ryuichi <ryucrosskey@gmail.com>
2018-08-17 19:04:28 +0900

    メモリサイズ取得関数追加

--- src/cpu_utilization_get.py
+++ src/cpu_utilization_get.py
@@ -1,6 +1,17 @@
 # -*- coding:utf-8 -*-
 import psutil
 
+# 変数のメモリサイズを取得する関数
+def print_varsize():
+    import types
+    print("{}{: >15}{}{: >10}{}".format('|','Variable Name','|','  Size','|'))
+    print(" -------------------------- ")
+    for k, v in globals().items():
+        if hasattr(v, 'size') and not k.startswith('_') and not isinstance(v,types.ModuleType):
+            print("{}{: >15}{}{: >10}{}".format('|',k,'|',str(v.size),'|'))
+        elif hasattr(v, '__len__') and not k.startswith('_') and not isinstance(v,types.ModuleType):
+            print("{}{: >15}{}{: >10}{}".format('|',k,'|',str(len(v)),'|'))
+
 # メモリとCPUの利用情報を取得
 memory = psutil.virtual_memory()
 cpu_percent = psutil.cpu_percent(interval=1)
